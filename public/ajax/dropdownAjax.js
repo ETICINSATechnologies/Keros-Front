5450b4bf8d6dd9e8f6cf78d52744fd7a7479aa38
Pabl01 <mexicain.du.01@gmail.com>
2019-01-10 22:02:23 +0100

    Files for paginate dropdown with Ajax

--- public/ajax/dropdownAjax.js
+++ public/ajax/dropdownAjax.js
@@ -1,16 +1,48 @@
-$("#test").click(function () {
-  $.get("/ua/contact/json", function (data) {
-    generateOptions(data);
-  });
-});
+function callAjax(source, number, id) {
+  source += "?pageNumber=" + number;        //RIGHT
+  $.get(source, function (data) {
+    generateButtons(data, id);
+    generateLinks(number, id, 5);          //WRONG
+    //generateLinks(data.meta.page, id, data.meta.totalPages);    //RIGHT
+  })
+}
 
-function generateOptions(data) {
-  console.log(data.content);
+function generateLinks(number, id, max) {
+  var links = "";
+  if (number == 0) {
+    links += "<a href='javascript:void(0)' onclick='callAjax(\"/ua/contact/json/\", " + (parseInt(number)+1) + ", \"" + id + "\")' class='fa fa-arrow-right fa-pull-right'></a>";
+  }
+  else if (number == max) {
+    links += "<a href='javascript:void(0)' onclick='callAjax(\"/ua/contact/json/\", " + (parseInt(number)-1) + ", \"" + id + "\")' class='fa fa-arrow-left fa-pull-left'></a>";
+  }
+  else {
+    links += "<a href='javascript:void(0)' onclick='callAjax(\"/ua/contact/json/\", " + (parseInt(number)-1) + ", \"" + id + "\")' class='fa fa-arrow-left fa-pull-left'></a>";
+    links += "<a href='javascript:void(0)' onclick='callAjax(\"/ua/contact/json/\", " + (parseInt(number)+1) + ", \"" + id + "\")' class='fa fa-arrow-right fa-pull-right'></a>";
+  }
+  console.log("link" + id);
+  $("#link" + id).empty().append(links);
+}
+
+function generateButtons(data, id) {
   var options = "";
-
+  var name = id.substring(14, id.length);
+  if (name.split("Id")[1] !== "1") {
+    options += "<button class='btn btn-block btn-flat' type='button' id='" + name + "_' onclick='selectValue(\"" + name + "_" + "\")'>---</button>";
+  }
   data.content.forEach(function (elem) {
-    options += "<option value='" + elem.id + "'>" + elem.firstName + " " + elem.lastName +"</option>";
+    options += "<button class='btn btn-block btn-flat' type='button' id='" + name + "_" + elem.id + "' onclick='selectValue(\"" + name + "_" + elem.id +"\")'>" + elem.firstName + " " + elem.lastName + "</button>";
   });
-  console.log(options);
-  return options;
+  $("#" + id).empty().append(options);
+}
+
+function selectValue(id) {
+  console.log(id);
+  var tmp = $("#" + id);
+  var grandParentId = tmp.parent().parent().attr("id");
+  var value = tmp.html();
+  var id = tmp.attr("id").split("_")[1];
+
+  $("a[href='#" + grandParentId + "'").html(value);
+  $("input[id='input" + grandParentId + "'").attr("value", id);
+  $(".collapse").collapse('hide');
 }
--- src/services/common/mock/contact/ContactMock.ts
+++ src/services/common/mock/contact/ContactMock.ts
@@ -25,18 +25,7 @@ export class ContactMock implements IMock {
     return null;
   }
   del<T>(resource: string, options?: IRequestOptions): IRestResponse<T> | null {
-      let status: number = 500;
-      switch (resource) {
-        case "ua/contact/1":
-          status = 204;
-          winston.debug("Contact 1 removed");
-          return new MockResponse(null, status);
-        case "ua/contact/2":
-          status = 204;
-          winston.debug("Contact 2 removed");
-          return new MockResponse(null, status);
-      }
-      return null;
+    return null;
   }
   get<T>(resource: string, options?: IRequestOptions): IRestResponse<T> | null {
     let mockObj: T | null = null;
@@ -51,7 +40,7 @@ export class ContactMock implements IMock {
         status = 200;
         return new MockResponse(mockObj, status);
       case "ua/contact":
-        mockObj = <T> new Page(<T[]> [new Contact(1, "Jimmy", "Neutron", new Gender(3, "A"), new Firm(2, "1111111", "La poissonerie des familles", new Address(2, "1204, rue des Acacias", "34, rue de Créqui", "69006", "Lyon", new Country(2, "Suisse")), new FirmType(4, "SARL")), "jimmy@test.com", "0450202122", "+33620547064", "Stagiaire", "Super sympa", true), new Contact(2, "José", "Bové", new Gender(4, "I"), new Firm(1, "99999999", "La boucherie du Léman", new Address(1, "37, rue des Lilas", "", "01220", "Grilly", new Country(1, "France")), new FirmType(2, "TPE/PME")), "votezjosé@test.com", "0405060708", "+33711121314", "PDG", "", false)], new Meta (0, 1, 2 , 25));
+        mockObj = <T> new Page(<T[]> [new Contact(1, "Jimmy", "Neutron", new Gender(3, "A"), new Firm(2, "1111111", "La poissonerie des familles", new Address(2, "1204, rue des Acacias", "34, rue de Créqui", "69006", "Lyon", new Country(2, "Suisse")), new FirmType(4, "SARL")), "jimmy@test.com", "0450202122", "+33620547064", "Stagiaire", "Super sympa", true), new Contact(2, "José", "Bové", new Gender(4, "I"), new Firm(1, "99999999", "La boucherie du Léman", new Address(1, "37, rue des Lilas", "", "01220", "Grilly", new Country(1, "France")), new FirmType(2, "TPE/PME")), "votezjosé@test.com", "0405060708", "+33711121314", "PDG", "", false)], new Meta (0, 5, 2 , 25));
         status = 200;
         return new MockResponse(mockObj, status);
     }
