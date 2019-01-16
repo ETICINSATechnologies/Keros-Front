c50ed70fb3abefe8eb491281d133f02c5076fea3
Pabl01 <mexicain.du.01@gmail.com>
2019-01-16 21:46:10 +0100

    Ajax update again

--- public/ajax/ajaxSelect.js
+++ public/ajax/ajaxSelect.js
@@ -1,29 +1,36 @@
 $('document').ready( function() {
-  $.get("/ua/contact/json", function (data) {
-    generateOptions($(".selectpicker.selectcontacts"), data, true);
-  });
-  $.get("/core/member/json", function (data) {
-    generateOptions($(".selectpicker.selectmembers"), data, true);
-  });
+  if ($(".selectpicker.selectcontacts").length > 1) {
+    $.get("/ua/contact/json", function (data) {
+      generateOptions($(".selectpicker.selectcontacts"), data, true);
+    });
+  }
+  if ($(".selectpicker.selectmembers").length > 1) {
+    $.get("/core/member/json", function (data) {
+      generateOptions($(".selectpicker.selectmembers"), data, true);
+    });
+  }
 });
 
 $('body').on('change','.selectpicker', function () {
+  // Si l'option sélectionnée n'est pas un lien de pagination, alors on ne fait rien (pour l'instant)
   if (!($(this).val().match(/link/))) {return;}
 
-  var select_menu = $(this);
-  var pageNumber = parseInt($(this).val().split("_")[1]);
+  let select_menu = $(this);
+  let params = {};
+  params.pageNumber = $(this).val().split("_")[1];
 
-  $.get($(this).attr("data-endpoint") + "/json?pageNumber=" + pageNumber, function (data) {
+  $.get($(this).attr("data-endpoint") + "/json?" + $.param(params), function (data) {
     generateOptions(select_menu, data);
   })
 });
 
 function generateOptions(select_menu, data, init) {
-  select_menu.empty().filter(function () {return !($(this).attr('class').match(/required/));}).append("<option></option>");
+  select_menu.empty().not($(".required")).append("<option></option>");
 
   data.content.forEach(function (elem) {
     select_menu.each( function () {
-      if (elem.id == $(this).attr('data-selected')) {var selected = "selected";}
+      let selected = "";
+      if (elem.id == $(this).attr('data-selected')) {selected = "selected";}
       $(this).append("<option value='" + elem.id + "' "+ selected +">" + elem.firstName + " " + elem.lastName + "</option>");
     });
   });
@@ -33,18 +40,18 @@ function generateOptions(select_menu, data, init) {
   if (!init) {select_menu.addClass('open').selectpicker('setStyle');}
 }
 
-function paginate($, meta) {
+function paginate(select, meta) {
   if (meta.totalPages === 1) {
-    return $;
+    return select;
   }
   else if (meta.page === 0) {
-    return $.append("<option value='link_"+ (meta.page + 1) +"' data-icon=\"fa fa-arrow-right fa-pull-right\"></option>");
+    return select.append("<option value='link_"+ (meta.page + 1) +"' data-icon=\"fa fa-arrow-right fa-pull-right\"></option>");
   }
   else if (meta.page === meta.totalPages - 1) {
-    return $.append("<option value='link_"+ (meta.page - 1) +"' data-icon=\"fa fa-arrow-left fa-pull-left\"></option>");
+    return select.append("<option value='link_"+ (meta.page - 1) +"' data-icon=\"fa fa-arrow-left fa-pull-left\"></option>");
   }
   else {
-    return $.append("<option value='link_"+ (meta.page - 1) +"' class='selectlinks' data-icon=\"fa fa-arrow-left fa-pull-left\" style='position: absolute; width: 50%; z-index: 9999;'></option>" +
+    return select.append("<option value='link_"+ (meta.page - 1) +"' class='selectlinks' data-icon=\"fa fa-arrow-left fa-pull-left\" style='position: absolute; width: 50%; z-index: 9999;'></option>" +
       "<option value='link_"+ (meta.page + 1) +"' class='selectlinks' data-icon=\"fa fa-arrow-right fa-pull-right\" style='width: 50%; margin-left: 50%;'></option>");
   }
 }
