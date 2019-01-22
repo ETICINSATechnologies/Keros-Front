8567a872551584e981d2c4de4113deabf1ece42f
VincentD <45464593+vdevulder1@users.noreply.github.com>
2019-01-23 00:50:55 +0100

    Limit. sélection dans étude (chadaff, contact,...)

--- public/ajax/ajaxSelect.js
+++ public/ajax/ajaxSelect.js
@@ -6,33 +6,72 @@ $('document').ready( function() {
   }
   if ($(".selectpicker.selectmembers").length > 1) {
     $.get("/core/member/json", function (data) {
+      data.content = data.content.filter(function(el){
+        return el.positions.some(function(el) {
+          return el == "Consultant";
+        });
+      });
       generateOptions($(".selectpicker.selectmembers"), data, true);
+    });
+  }
+  if ($(".selectpicker.selectmembers.selectleaders").length > 1) {
+    $.get("/core/member/json", function (data) {
+      data.content = data.content.filter(function(el){
+        return el.positions.some(function(el) {
+          return el.pole.name == "Chargé d'affaire";
+        });
+      });
+      generateOptions($(".selectpicker.selectmembers.selectleaders"), data, true);
+    });
+  }
+  if ($(".selectpicker.selectmembers.selectPerfleaders").length > 1) {
+    $.get("/core/member/json", function (data) {
+      data.content = data.content.filter(function(el){
+        return el.positions.some(function(el) {
+          return el.pole.name == "Performance";
+        });
+      });
+      generateOptions($(".selectpicker.selectmembers.selectPerfleaders"), data, true);
     });
   }
 });
 
-$('body').on('change','.selectpicker', function () {
-  // Si l'option sélectionnée n'est pas un lien de pagination, alors on ne fait rien (pour l'instant)
-  if (!($(this).val().match(/link/))) {return;}
+$('body').on('change','.selectpicker , .form-control', function () {
 
-  let select_menu = $(this);
-  let params = {};
-  params.pageNumber = $(this).val().split("_")[1];
+  if($(this).attr("id") == "firmId"){
+    let selectedFirm = $(this).find('option:selected').text();
+    $.get("/ua/contact/json", function (data) {
+      data.content = data.content.filter(function(el) {
+        return el.firm.name == selectedFirm;
+      });
+      generateOptions($(".selectpicker.selectcontacts"), data, true);
+    });
+  }
 
-  $.get($(this).attr("data-endpoint") + "/json?" + $.param(params), function (data) {
+  if ($(this).val().match(/link/)) {
+    let select_menu = $(this);
+    let params = {};
+    params.pageNumber = $(this).val().split("_")[1];
+
+    $.get($(this).attr("data-endpoint") + "/json?" + $.param(params), function (data) {
     generateOptions(select_menu, data);
   })
+  }
+
+
 });
 
 function generateOptions(select_menu, data, init) {
   select_menu.empty().not($(".required")).append("<option></option>");
 
   data.content.forEach(function (elem) {
-    select_menu.each( function () {
-      let selected = "";
-      if (elem.id == $(this).attr('data-selected')) {selected = "selected";}
-      $(this).append("<option value='" + elem.id + "' "+ selected +">" + elem.firstName + " " + elem.lastName + "</option>");
-    });
+      select_menu.each(function () {
+        let selected = "";
+        if (elem.id == $(this).attr('data-selected')) {
+          selected = "selected";
+        }
+        $(this).append("<option value='" + elem.id + "' " + selected + ">" + elem.firstName + " " + elem.lastName + "</option>");
+      });
   });
   paginate(select_menu, data.meta);
 
