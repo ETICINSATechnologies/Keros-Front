9b7822ee2099458a815decbb896a42fa606ce4ea
oliviacaraiman <caraimanolivia@gmail.com>
2019-01-31 11:55:16 +0100

    checkbox Cofindentiel desactive en mode "view"

--- public/js/formButtons.js
+++ public/js/formButtons.js
@@ -1,5 +1,6 @@
 function editButtonPress() {
   $('form :input').prop('readonly', false);
+  $('#confidential').prop('disabled', false);
   $('#view-buttons').hide();
   $('#view-mailshots').hide();
   $('#update-buttons').show();
