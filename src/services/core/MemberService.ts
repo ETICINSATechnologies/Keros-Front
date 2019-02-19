5d3747fc061587f5d15a6d14e0da294fdd9d49e1
vdevulder1 <45464593+vdevulder1@users.noreply.github.com>
2019-02-19 13:56:25 +0100

    Bug fix from rebasing branch earlier

--- src/services/core/MemberService.ts
+++ src/services/core/MemberService.ts
@@ -4,6 +4,7 @@ import { BaseService } from '../common/BaseService';
 import { Page } from '../../models/core/Page';
 import { MemberCreateRequest } from '../../models/core/MemberCreateRequest';
 import * as winston from 'winston';
+import { queryStringify } from "../../util/Helper";
 
 export class MemberService extends BaseService {
   static getMember(id: number, callback: (err: any, result: Member | null) => void): void {
@@ -35,46 +36,14 @@ export class MemberService extends BaseService {
   }
 
   static getAllMembers(callback: (err: any, result: Page<Member> | null) => void, queryParams?: any): void {
-    let queryString = "";
-    if(queryParams && Object.keys(queryParams).length !== 0) {
-      let paramTuples: any[] = [];
-      for (const key in queryParams) {
-        const value = queryParams[key];
-        paramTuples.push([key, value].join("="));
-      }
-      queryString = "?" + paramTuples.join("&");
-    }
-
-    this.rest.get<Page<Member>>('core/member' + queryString, this.defaultHeaders()).then(
+    const queryString = queryStringify(queryParams);
+    this.rest.get<Page<Member>>("core/member?" + queryString, this.defaultHeaders()).then(
       (res: IRestResponse<Page<Member>>) => {
         if (res.statusCode !== 200) {
           return callback(this.defaultError(res.statusCode), null);
         }
-        winston.debug('Response : ' + JSON.stringify(res));
-        let totalPages = 1;
-        if (res.result && res.result.meta && res.result.meta.totalPages) {
-          totalPages = res.result.meta.totalPages;
-        }
-        if (totalPages !== 1) {
-          let pageCount = 1;
-          while (totalPages - pageCount !== 0) {
-            this.rest.get<Page<Member>>("core/member?pageNumber=" + pageCount + queryString, this.defaultHeaders()).then(
-              (resu: IRestResponse<Page<Member>>) => {
-                if (resu.statusCode !== 200) {
-                  return callback(this.defaultError(resu.statusCode), null);
-                }
-                winston.debug('Response for page ' + pageCount + ' : ' + JSON.stringify(resu));
-                if (res.result && res.result.content &&  resu.result && resu.result.content) {
-                  res.result.content = res.result.content.concat(resu.result.content);
-                  callback(null, res.result);
-                }
-              }).catch(e => callback(e, null)
-            );
-            pageCount += 1;
-          }
-        } else {
-          callback(null, res.result);
-        }
+        winston.debug("Response : " + JSON.stringify(res));
+        callback(null, res.result);
       }
     ).catch(
       e => callback(e, null)
