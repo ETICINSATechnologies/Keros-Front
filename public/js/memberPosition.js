d16e43abbe8a990bfea8befd8767dec21bc837ea
oliviacaraiman <caraimanolivia@gmail.com>
2019-03-06 16:51:00 +0100

    Merge branch 'develop' of github.com:ETICINSATechnologies/Keros-Front into devbranch
    # Conflicts:
    #	src/control/core/MemberController.ts
    #	src/models/core/MemberCreateRequest.ts
    #	src/services/common/mock/member/MemberMock.ts

--- public/js/memberPosition.js
+++ public/js/memberPosition.js
@@ -3,13 +3,16 @@ $('#yearPosition1').attr({"max": currentYear});
 $('#yearPosition2').attr({"max": currentYear});
 $('#yearPosition3').attr({"max": currentYear});
 
-if ($('#yearPosition1').val().length === 0) {
-  $('#yearPosition1').val(currentYear);
-}
-if ($('#yearPosition2').val().length === 0) {
+try {
+  if ($('#yearPosition1').val().length === 0) {
+    $('#yearPosition1').val(currentYear);
+  }
+  if ($('#yearPosition2').val().length === 0) {
   $('#yearPosition2').val(currentYear);
-}
-if ($('#yearPosition3').val().length === 0) {
+  }
+  if ($('#yearPosition3').val().length === 0) {
   $('#yearPosition3').val(currentYear);
+  }
+} catch {
 }
 
