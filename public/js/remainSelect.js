4b55582f9c4a1ecbba9f903e3b343e0f407284f3
ZHANG Mengxin <untraveledz@gmail.com>
2020-03-08 15:17:24 +0100

    delete some comments, change variable name

--- public/js/remainSelect.js
+++ public/js/remainSelect.js
@@ -1,9 +1,6 @@
 function savePositionSelectIndex(){
     let position=document.getElementById("positions-select");
     let positionChoice=position.options[position.selectedIndex].value;
-    //setting cookies
-    //member?posisionId=1
-    //member
     document.cookie='positionChoice='+positionChoice;
     document.cookie='poleChoice='+0;
     self.location.href=position.options[position.selectedIndex].value;
@@ -12,60 +9,47 @@ function savePositionSelectIndex(){
 function savePoleSelectIndex(){
     let pole=document.getElementById("pole-select");
     let poleChoice=pole.options[pole.selectedIndex].value;
-    //setting cookies
-    //member?posisionId=1
-    //member
     document.cookie='poleChoice='+poleChoice;
     document.cookie='positionChoice='+0;
     self.location.href=pole.options[pole.selectedIndex].value;
 }
 
-
 function selectIndex(){
-    console.log('loading select index');
     //remember to initialize
     let positionChoice=0;
     let poleChoice=0;
-    //getting cookies
-    let coosStr=document.cookie;
-    console.log(coosStr);
-    //splitting by ; and space
-    let coos=coosStr.split("; ");
-    for(let i=0;i<coos.length;i++){
-        //splitting by =
-        let coo=coos[i].split("=");
-        console.log(coo);
-        if(coo[0]=="positionChoice"){
-            if(coo[2]===undefined){
-                positionChoice='all';//all members or all consultants
+    let cookieString =document.cookie;
+    let cookies=cookieString .split("; ");
+    for(let i=0;i<cookies.length;i++){
+        let cookie=cookies[i].split("=");
+        if(cookie[0]=="positionChoice"){
+            if(cookie[2]===undefined){
+                positionChoice='all';//all members
             }
             else
             {
-                positionChoice = coo[2];
+                positionChoice = cookie[2];
             }
         }
-        if(coo[0]=="poleChoice"){
-            if(coo[2]===undefined){
-                poleChoice='all';//all members or all consultants
+        if(cookie[0]=="poleChoice"){
+            if(cookie[2]===undefined){
+                poleChoice='all';//all members
             }
             else
             {
-                poleChoice = coo[2];
+                poleChoice = cookie[2];
             }
-
         }
     }
-
 
     let position=document.getElementById("positions-select");
     let pole=document.getElementById("pole-select");
 
     //POSITION
-    //not chose
     if(positionChoice==0) {
-        position.selectedIndex = 0;
+        position.selectedIndex = 0; //choix par defaut
     }else if(positionChoice=='all'){
-        position.selectedIndex = 1;
+        position.selectedIndex = 1; //AFFICHER TOUT
     } else{
         let length=position.options.length;
         for(let i=0;i<length;i++){
@@ -77,11 +61,10 @@ function selectIndex(){
     }
 
     //POLE
-    //not chose
     if(poleChoice==0) {
-        pole.selectedIndex = 0;
+        pole.selectedIndex = 0;//choix par defaut
     }else if(poleChoice=='all'){
-        pole.selectedIndex= 1;
+        pole.selectedIndex= 1;//AFFICHER TOUT
     }else{
         let length=pole.options.length;
         for(let i=0;i<length;i++){
